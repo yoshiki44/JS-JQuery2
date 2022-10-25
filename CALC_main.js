525d213370253da13ba82f19c1b131fa180f13e1
EC2 Default User <ec2-user@ip-172-31-27-12.ap-northeast-1.compute.internal>
2022-10-25 14:23:12 +0000

    新規登録

---
+++ CALC_main.js
@@ -0,0 +1,44 @@
+let display = document.getElementById("display");
+let equalCount = 0;
+
+function numberClick(target) {
+    let targetValue = target.innerHTML;
+    if (equalCount >= 1){
+        display.innerHTML = "0";
+        equalCount = 0;
+    }
+    
+    if(display.innerHTML == "0" && targetValue =="0"){
+        display.innerHTML ="0";
+    } else if(display.innerHTML == "0" && targetValue =="00"){
+        display.innerHTML ="0";
+    } else if(display.innerHTML == "0" && targetValue == "."){
+        display.innerHTML = "0.";
+    } else if(display.innerHTML == "0"){
+        display.innerHTML = targetValue;
+    } else{
+        display.innerHTML += targetValue;
+    }
+
+};
+
+function formulaClick(formula){
+    equalCount = 0;
+    let formulaValue = formula.innerHTML;
+    
+    if(display.innerHTML.slice(-1) =="+" ||display.innerHTML.slice(-1) =="-" ||display.innerHTML.slice(-1) =="*" ||display.innerHTML.slice(-1) =="/"){
+        display.innerHTML = display.innerHTML.slice(0,-1) + formula.innerHTML;
+    }else{
+        display.innerHTML += formulaValue;
+    }
+};
+
+function clearClick(){
+    equalCount = 0;
+    display.innerHTML = "0";
+};
+
+function equalClick(){
+    equalCount += 1;
+    display.innerHTML = Math.round(eval(display.innerHTML)*100000)/100000;
+};
